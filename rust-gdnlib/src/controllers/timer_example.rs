9ae32de44c359301c075fa0abb8c5ba365fc029d
Gabriel Vilches <gabriel.vilches@xero.com>
2021-12-31 09:01:36 +1300

    import dodge creeps

--- rust-gdnlib/src/controllers/timer_example.rs
+++ rust-gdnlib/src/controllers/timer_example.rs
@@ -1,19 +1,20 @@
+use gdnative::prelude::NodeExt;
 use gdnative::prelude::*;
 
 #[derive(NativeClass)]
 #[inherit(Node2D)]
 pub struct TimerExample;
 
-fn get_cast<'a, T>(owner: TRef<'a, Node2D>, path: &str) -> TRef<'a, T>
-where
-    T: SubClass<Node>,
-{
-    let node = owner.get_node(path).expect("couldn't get node");
-    let node = unsafe { node.assume_safe() };
-    let node = node.cast::<T>().expect("couldn't cast");
+// fn get_cast<'a, T>(owner: TRef<'a, dyn SubClass<dyn NodeExt, RefKind = dyn gdnative::GodotObject>>, path: &str) -> TRef<'a, T>
+// where
+//     T: SubClass<Node>,
+// {
+//     let node = owner.get_node(path).expect("couldn't get node");
+//     let node = unsafe { node.assume_safe() };
+//     let node = node.cast::<T>().expect("couldn't cast");
 
-    node
-}
+//     node
+// }
 
 #[methods]
 impl TimerExample {
@@ -23,7 +24,15 @@ impl TimerExample {
 
     #[export]
     fn _ready(&self, owner: TRef<Node2D>) {
-        let timer = get_cast::<Timer>(owner, "Timer");
+        // let timer = get_cast::<Timer>(owner, "Timer");
+
+        let timer = unsafe {
+            (&*owner)
+                .get_node_as::<Timer>("Timer")
+                .expect("couldn't get the node")
+        };
+
+        // let timer = unsafe { (&*owner).get_node_as::<Timer>("Timer") };
 
         timer
             .connect(
@@ -40,7 +49,13 @@ impl TimerExample {
 
     #[export]
     fn _on_timer_timeout(&self, owner: TRef<Node2D>) {
-        let sprite = get_cast::<Sprite>(owner, "Sprite");
+        // let sprite = get_cast::<Sprite>(owner, "Sprite");
+        let sprite = unsafe {
+            (&*owner)
+                .get_node_as::<Sprite>("Sprite")
+                .expect("couldn't get the node")
+        };
+
 
         let is_visible = sprite.is_visible();
 
