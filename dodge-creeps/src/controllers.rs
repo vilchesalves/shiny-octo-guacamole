da46e5ef0ccc537f0bf76ffe381def210da09f06
Gabriel Vilches <gabriel.vilches@xero.com>
2022-01-03 08:44:42 +1300

    initial control implementation

--- dodge-creeps/src/controllers.rs
+++ dodge-creeps/src/controllers.rs
@@ -1,9 +1,11 @@
-use gdnative::prelude::*;
 use gdnative::api::Area2D;
+use gdnative::prelude::*;
 
 #[derive(NativeClass)]
 #[inherit(Area2D)]
 pub struct PlayerController {
+    screen_size: Option<Size2>,
+
     #[property(default = 400.0)]
     speed: f32,
 }
@@ -11,7 +13,8 @@ pub struct PlayerController {
 impl PlayerController {
     fn new(_owner: &Area2D) -> Self {
         Self {
-            speed: 400.0
+            screen_size: None,
+            speed: 400.0,
         }
     }
 }
@@ -19,7 +22,52 @@ impl PlayerController {
 #[methods]
 impl PlayerController {
     #[export]
-    fn _ready(&self, _owner: TRef<Area2D>) {
-        
+    fn _ready(&mut self, owner: &Area2D) {
+        self.screen_size = Some(owner.get_viewport_rect().size);
+    }
+
+    #[export]
+    fn _process(&self, owner: &Area2D, delta: f32) {
+        let input = Input::godot_singleton();
+
+        let mut velocity = Vector2::new(0.0, 0.0);
+
+        if input.is_action_pressed(GodotString::from_str("ui_up")) {
+            velocity.y -= 1.0;
+        }
+
+        if input.is_action_pressed(GodotString::from_str("ui_right")) {
+            velocity.x += 1.0;
+        }
+
+        if input.is_action_pressed(GodotString::from_str("ui_down")) {
+            velocity.y += 1.0;
+        }
+
+        if input.is_action_pressed(GodotString::from_str("ui_left")) {
+            velocity.x -= 1.0;
+        }
+
+        if velocity.length() == 0.0 {
+            return;
+        }
+
+        velocity = velocity.normalize() * self.speed;
+
+        let global_position = owner.global_position();
+        let position_delta = velocity * delta;
+        let position = global_position + position_delta;
+
+        match self.screen_size {
+            Some(s) => {
+                owner.set_global_position(Vector2::new(
+                    position.x.max(0.0).min(s.width),
+                    position.y.max(0.0).min(s.height),
+                ));
+            }
+            None => {
+                godot_print!("screen is not defined");
+            }
+        }
     }
 }
