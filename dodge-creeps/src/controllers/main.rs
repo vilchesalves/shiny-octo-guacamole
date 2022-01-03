726ed049a19e34b01e8386e3526805d6495fd7ab
Gabriel Vilches <gabriel.vilches@xero.com>
2022-01-04 09:12:55 +1300

    define game_over

--- dodge-creeps/src/controllers/main.rs
+++ dodge-creeps/src/controllers/main.rs
@@ -23,18 +23,11 @@ fn connect_player(owner: &Node) {
             .expect("couldn't get player node")
     };
 
-    let target = unsafe {
-        owner.get_node_as::<Node>(".")
-        .expect("couldn't get target")
-    };
-    
-    player.connect(
-        "hit",
-        target,
-        "game_over",
-        VariantArray::new_shared(),
-        0,
-    ).expect("couldn't connect hit");
+    let target = unsafe { owner.get_node_as::<Node>(".").expect("couldn't get target") };
+
+    player
+        .connect("hit", target, "game_over", VariantArray::new_shared(), 0)
+        .expect("couldn't connect hit");
 }
 
 #[methods]
@@ -42,5 +35,21 @@ impl Main {
     #[export]
     fn _ready(&self, owner: &Node) {
         connect_player(owner);
+    }
+
+    #[export]
+    fn new_game(&self, owner: &Node) {}
+
+    #[export]
+    fn game_over(&self, owner: &Node) {
+        let score_timer = unsafe {
+            owner.get_node_as::<Timer>("ScoreTimer").expect("couldn't get score timer")
+        };
+        score_timer.stop();
+
+        let mob_timer = unsafe {
+            owner.get_node_as::<Timer>("MobTimer").expect("couldn't get mob timer")
+        };
+        mob_timer.stop();
     }
 }
